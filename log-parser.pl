578d65b1b23eaa171d5d8d3c1f9c1b42a5f3a9e9
Philip Grenon <pdgrenon@gmail.com>
2017-07-03 16:11:01 -0400

    Initial commit of log-parser; add test data to the generator
    Needed to get log-parser into version control. Also needed new data to
    test it.

---
+++ log-parser.pl
@@ -0,0 +1,47 @@
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $directory = './testData';
+my $args = GetOptions("directory|d=s" => \$directory);
+# ASSUMPTION: logs will be rolled into logs that are archived by the earliest hour in the log, not the latest, meaning there will be logs from 00-23
+# Example: the log from 1 PM to 2 PM will be stored as YYYY_MM_DD_13
+
+# ASSUMPTION: This doesn't need precision down to the milliseconds/nanoseconds
+my $currentTime=`date +%Y_%m_%d_%H_%M_%S`;
+chomp $currentTime;
+my(@regexMatch) = $currentTime =~ /(.*_.*_.*_.*)_(.*)_(.*)/;
+
+my $currentHour = $regexMatch[0];
+my $secondCount = ($regexMatch[1] * 60 + $regexMatch[2]);
+my $tenMinutesAgo = $secondCount - 600;
+if ($tenMinutesAgo < 0){
+	$tenMinutesAgo = 0;
+}
+print "ten minutes ago cutoff: $tenMinutesAgo\n"
+
+opendir (my $dirHandle, $directory) || die "Unable to open directory $directory: $!";
+while (readdir $dirHandle) {
+	my $logfile = $_;
+		if ( $logfile =~ $currentHour ) {
+			open (my $fileHandle, '<', "$directory/$logfile") or die "Unable to open file, $!";
+			while (<$fileHandle>) {
+				my $line = $_;
+				my @fields = split(/ /,$line);
+				# Grab the minutes and seconds from the time field
+				my(@fieldsMatch) = $fields[3] =~ /.*:\d\d:(\d\d):(\d\d)/;
+				my $lineTime = ($fieldsMatch[0]*60 + $fieldsMatch[1]);
+				if ($lineTime >= $tenMinutesAgo ) {
+						print "line time: $lineTime\n"
+						if( $fields[8] == '500') {
+							print "$line";
+						}
+				}
+			}
+			close $fileHandle
+		}
+}
+
+closedir $dirHandle;
--- testData/testDataGenerator.pl
+++ testData/testDataGenerator.pl
@@ -12,11 +12,16 @@ for (my $i = 0; $i <= 23; $i++ ) {
 	# prepend a zero to the single digits
 	$fileName .= ($i < 10 ? "_0".$i : "_".$i);
 	open (my $fileHandle, '>', $fileName);
-	print $fileHandle <<'END';
-127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
-127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "POST /apache_pb.gif HTTP/1.0" 330 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+	print $fileHandle <<"END";
+127.0.0.1 - frank [10/Oct/2000:16:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:16:55:36 -0700] "POST /apache_pb.gif HTTP/1.0" 330 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "PUT /apache_pb.gif HTTP/1.0" 404 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
-127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:16:06:59 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:16:06:00 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:16:05:36 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:16:00:36 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:15:56:59 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+127.0.0.1 - frank [10/Oct/2000:15:56:00 -0700] "DELETE /apache_pb.gif HTTP/1.0" 500 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
 END
 
 	close $fileHandle;
