1bdcda854f32ded3a8d69e9e5fee75d9daa3634f
Philip Grenon <pdgrenon@gmail.com>
2017-07-03 11:44:16 -0400

    Initial commit
    This script is meant to copy specific log files (those occurring from 6PM
    to 9PM) from a local server to a remote server. The script takes
    configuration options at run time, and could easily be configured to use
    default values as well.

---
+++ log-parser.pl
@@ -0,0 +1,50 @@
+# This script assumes that ssh keys have been exchanged between the remote server and the server from which
+# this script will be run
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $server = ''; #= "0.0.0.0"; # Use this variable to set the remote server (where the logs will be copied)
+my $directory = "."; # Use this variable to set the directory where the log files will be copied from (defaults to the current directory)
+my $remoteDirectory = "~/logs";
+my $args = GetOptions("server|s=s" => \$server,
+											"directory|d=s" => \$directory,
+											"remoteDirectory|r=s" => \$remoteDirectory,
+											"help|s" => \&usage)
+											or die ("Error in command line arguments");
+
+if (!$server) {
+	print "No remote server given. Exiting...\n";
+	exit(1);
+}
+
+sub usage {
+	my $usage = <<'END';
+Usage:
+	log-parser --server <Remote Server> [--directory --remoteDirectory] [--help]
+
+	where:
+		--directory is the local log directory
+		--remoteDirectory is the remote log directory
+		-- help prints this usage information
+END
+print $usage;
+exit(0);
+}
+
+# ASSUMPTION: logs will be rolled into logs that are archived by the earliest hour in the log, not the latest, meaning there will be logs from 00-23
+# Example: the log from 1 PM to 2 PM will be stored as YYYY_MM_DD_13
+my $currentDate=`date +%Y_%m_%d`;
+chomp $currentDate;
+
+opendir (my $handler, $directory) || die "Unable to open directory $directory: $!";
+while (readdir $handler) {
+	if ($_ =~ /$currentDate\_1[89]/ || $_ =~ /$currentDate\_2[0]/) {
+			#system ("scp -i ~/id_rsa $directory/$_ $server:$remoteDirectory");
+			print "$_ would be copied\n";
+	}
+}
+
+closedir $handler;
